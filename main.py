dd4e53d0bb205ce0ef8f98191912659148cbd333
SabbaKix <9255011+sabbakix@users.noreply.github.com>
2021-02-25 12:10:24 +0100

    Crea files zip contenenti al massimo 10 files

--- main.py
+++ main.py
@@ -2,6 +2,12 @@
 import os
 from os.path import join
 from glob import glob
+import zipfile
+
+CF = input("Inserisci CF: ") 
+print(CF) 
+AP = input("Inserisci (A) per Fatture Attive (P) per passive: ") 
+print(AP) 
 
 # rimuovi i files di metadati
 base_dir = 'C://xdata/'
@@ -17,15 +23,33 @@ for ext in ('*.xml', '*.xml.p7m'):
 
 print(files)
 
+
+
 # crea dei files zip di contenente al massimo 10 files xml
 counter = 1
 nfiles = len(files)
 counter_gruppo = 1
+gruppo = []
+
 for file in files:
-    gruppo = []
     print(counter, end=': ')
-    print(file)
+    #print(file)
+    gruppo.append(file)
+
     if counter %10 == 0 or counter == nfiles:
-        print ('gruppo:'+str(counter_gruppo)+' fine gruppo fai zip')
+        nome_file_zip = 'IT'+CF+'_19'+AP+str(counter_gruppo).zfill(2)+'.zip'
+        ziph = zipfile.ZipFile(base_dir+nome_file_zip, 'w', zipfile.ZIP_DEFLATED)
+
+        print(gruppo)
+        for file_nel_gruppo in gruppo:
+            print('file_nel_gruppo', end=': ')
+            print(os.path.basename(file_nel_gruppo))
+            ziph.write(file_nel_gruppo,os.path.basename(file_nel_gruppo))
+        ziph.close()
+        print ('gruppo 2019:'+str(counter_gruppo)+' fine gruppo fai zip : '+nome_file_zip)
+        gruppo = []
         counter_gruppo += 1
+
     counter += 1
+
+
