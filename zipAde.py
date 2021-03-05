9122ccceb4411c8f7211ddcea43407fcd6d8983a
SabbaKix <9255011+sabbakix@users.noreply.github.com>
2021-03-05 15:19:04 +0100

    Moved funtionalities to the GUi

--- zipAde.py
+++ zipAde.py
@@ -5,6 +5,10 @@ zipAde Interfaccia principale.
 
 
 import sys
+import os
+from os.path import join
+from glob import glob
+import zipfile
 import tempfile
 
 #tkinter library
@@ -17,7 +21,14 @@ import tkinter.messagebox
 
 class App:
 
-    temp_dir = tempfile.TemporaryDirectory()
+    base_dir = "C:/xdata"
+    temp_dir = 0
+
+    CF = ""
+    origineZip = False
+    AP = ""
+
+
 
     def __init__(self, root):
 
@@ -66,7 +77,7 @@ class App:
         self.entry01["font"] = ft
         self.entry01["fg"] = "#333333"
         self.entry01["justify"] = "left"
-        self.entry01["text"] = "Entry"
+        self.entry01.insert(0,self.base_dir)
         self.entry01["relief"] = "flat"
         self.entry01["borderwidth"] = 2
         self.entry01["relief"] = "groove"
@@ -74,20 +85,17 @@ class App:
         self.entry01.configure(state=tk.DISABLED)
         self.entry01.config(fg = 'blue')
 
-        
-
         self.listbox01=tk.Listbox(root)
         self.listbox01["borderwidth"] = "1px"
         ft = tkFont.Font(family='Times',size=10)
         self.listbox01["font"] = ft
         self.listbox01["fg"] = "#333333"
         self.listbox01["justify"] = "left"
-        self.listbox01.place(x=10,y=250,width=760,height=301)
+        self.listbox01.place(x=10,y=450,width=760,height=101)
         self.listbox01["yscrollcommand"] = True
 
-        
-        self.scrollbar = tk.Scrollbar(root) 
-        self.scrollbar.pack(side = tk.RIGHT, fill = tk.BOTH) 
+        self.scrollbar = tk.Scrollbar(self.listbox01) 
+        self.scrollbar.pack(side = tk.RIGHT, fill = tk.BOTH)
         self.scrollbar.config(command = self.listbox01.yview) 
         self.listbox01.config(yscrollcommand = self.scrollbar.set)
 
@@ -100,7 +108,6 @@ class App:
         self.avviaButton["text"] = "Avvia"
         self.avviaButton.place(x=660,y=560,width=111,height=30)
         self.avviaButton["command"] = self.eseguiAvviaButton
-
 
         self.clearButton=tk.Button(root)
         self.clearButton["bg"] = "#efefef"
@@ -121,9 +128,6 @@ class App:
         self.esciButton["text"] = "Esci"
         self.esciButton.place(x=400,y=560,width=121,height=30)
         self.esciButton["command"] = self.eseguiEsciButton
-
-        
-
 
         self.cflabel=tk.Label(root)
         ft = tkFont.Font(family='Times',size=10)
@@ -179,22 +183,27 @@ class App:
         ft = tkFont.Font(family='Times',size=10)
         self.textbox01["font"] = ft
         self.textbox01["fg"] = "#333333"
-        #["justify"] = "left"
         self.textbox01["relief"] = "groove"
-        self.textbox01.place(x=11,y=120,width=760,height=122)
-        self.textbox01.insert(tk.INSERT, "testo dtestoesto dtesto dtesto dtesto esto dtesto dtesto ")
+        self.textbox01.place(x=11,y=120,width=760,height=322)
+        self.textbox01.insert(tk.INSERT, "Seleziona la cartella con le fatture, compila il campo Codice Fiscale, e premi Avvia\n")
         self.textbox01.configure(state=tk.DISABLED)
+        
+        self.scrollbar02 = tk.Scrollbar(self.textbox01) 
+        self.scrollbar02.pack(side = tk.RIGHT, fill = tk.Y)
+        self.scrollbar02.config(command = self.textbox01.yview) 
+        self.textbox01.config(yscrollcommand = self.scrollbar02.set)
+        
 
     def apradiobutton01_command(self):
-        print("command")
+        self.AP = 'A'
 
 
     def apradiobutton02_command(self):
-        print("command")
+        self.AP = 'P'
 
 
     def checkbutton01_command(self):
-        print("command")
+        self.origineZip = True
 
         
 
@@ -204,6 +213,7 @@ class App:
         self.entry01.delete(0,tk.END)
         self.entry01.insert(0,dirName)
         print("dir: ",dirName)
+        self.base_dir = dirName
         self.entry01.configure(state=tk.DISABLED)
 
     def showMessage(self):
@@ -215,19 +225,112 @@ verranno lette le fatture negli zip e rezippate in pacchetti da 10 fatture.')
 
     def eseguiAvviaButton(self):
 
-        for x in range(10):
-            self.consolePrint(str(x)+"hello")
-
+        """
+        for x in range(100):
+            self.consolePrint(str(x)+" Test listbox")
         print("Avvia Button")
+        print(self.temp_dir.name)
+        self.dprint("testo in \n textbox test \n test nuovo")
+        """
+
+        self.temp_dir = tempfile.TemporaryDirectory()
+        print(self.temp_dir.name)
+
+        # Controllo che la dir temporanea sia vuota
+        for filepath in glob(self.temp_dir.name + '*.*'):
+            print('Controllo che la dir temporanea sia vuota: ', end='')
+            print(filepath)
+            if len(filepath) != 0:
+                self.dprint("Cartella temporanea non vuota")
+                return self.temp_dir.cleanup()
+
+        #Verifica cartella selezionata 
+        if self.base_dir == "":
+            self.dprint("Nessuna cartella selezionata.")
+            return self.temp_dir.cleanup()
+        
+        # Verifica CF
+        if len(self.cfentry02.get()) == 0:
+            self.dprint("Nessun Codice Fiscale inserito.")
+            return self.temp_dir.cleanup()
+
+
+        # Rileva la lista dei files nella directory dati
+        files_zip = []
+        files_zip.extend(glob(join(self.base_dir, '*.zip')))
+        if len(files_zip) == 0:
+            self.dprint("Nessun File Zip trovato nella cartella selezionata.")
+            return self.temp_dir.cleanup()
+
+        # Estrai i files Zip nella dir temporanea
+        for file_zip in files_zip:
+            print('Estrai i files Zip: ', end='')
+            print(file_zip)
+            with zipfile.ZipFile(file_zip,"r") as zip_ref:
+                zip_ref.extractall(self.temp_dir.name)
+
+        # Rimuovi i files di metadati
+        for filepath in glob(self.temp_dir.name + '\*_metaDato.xml'):
+            self.dprint('Ignora file di metadati: ', end='')
+            self.dprint(filepath)
+            os.remove(filepath)
+
+        self.dprint ("")
+
+        # Rileva la lista dei files rimasti nella directory temporanea
+        files = []
+        files.extend(glob(join(self.temp_dir.name, '*.*')))
+        #self.dprint("")
+        #self.dprint('Rileva la lista dei files rimasti nella directory temporanea: ')
+        #self.dprint(files)
+
+        # crea dei files zip contenenti al massimo 10 files
+        counter = 1
+        nfiles = len(files)
+        counter_gruppo = 1
+        gruppo = []
+        self.CF = self.cfentry02.get()
+        print(self.CF)
+        print(self.AP)
+        
+
+        for file in files:
+            self.dprint(counter, end=': ')
+            self.dprint(file)
+            gruppo.append(file)
+
+            if counter %10 == 0 or counter == nfiles:
+                nome_file_zip = 'IT'+self.CF+'_19'+self.AP+str(counter_gruppo).zfill(2)+'.zip'
+                ziph = zipfile.ZipFile(self.base_dir+'/'+nome_file_zip, 'w', zipfile.ZIP_DEFLATED)
+
+                #print(gruppo)
+                for file_nel_gruppo in gruppo:
+                    #self.dprint('file_nel_gruppo', end=': ')
+                    #self.dprint(str(os.path.basename(file_nel_gruppo)))
+                    ziph.write(file_nel_gruppo,os.path.basename(file_nel_gruppo))
+                ziph.close()
+                self.dprint ('inserite nel file: '+str(counter_gruppo)+' zip : '+nome_file_zip)
+                self.dprint ("")
+                gruppo = []
+                counter_gruppo += 1
+
+            counter += 1
+
+
+        # Stampa files preseti in cartella temporanea
+        for filepath in glob(self.temp_dir.name+'\*.*'):
+            pass
+            #self.dprint(filepath)
+            #os.remove(filepath)
+
+        self.temp_dir.cleanup()
+
 
         
-        print(self.temp_dir.name)
-        # use temp_dir
-        print('dir temparanea')
         
     
     def consolePrint(self,text=""):
-        output = str(self.item_num).zfill(3)+": "+text
+        output = str(self.item_num).zfill(3)+":  "+text
         self.listbox01.insert(self.item_num, output)
         self.listbox01.see(self.item_num)
         self.item_num += 1
@@ -240,9 +343,15 @@ verranno lette le fatture negli zip e rezippate in pacchetti da 10 fatture.')
     
     def eseguiEsciButton(self):
         print("Closing ...")
-        self.temp_dir.cleanup()
+        if self.temp_dir != 0:
+            self.temp_dir.cleanup()
         sys.exit()
     
+    def dprint(self,txt="", end="\n"):
+        self.textbox01.configure(state=tk.NORMAL)
+        self.textbox01.insert(tk.END, str(txt)+end)
+        self.textbox01.see(tk.END)
+        self.textbox01.configure(state=tk.DISABLED)
 
 if __name__ == "__main__":
     root = tk.Tk()
